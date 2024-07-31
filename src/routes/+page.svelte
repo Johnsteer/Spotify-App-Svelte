cd60dc2a41402f0c91149d3efeacf28c0a0416d5
John Steer <johnsteer3@gmail.com>
2024-07-30 23:36:21 -0400

    started testing styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="parent" style="display:flex; border:solid 1px black; overflow:hideen; height:100vh;">
+    <div id="sidebar" class="sidebar">
+        <h1>sidebar</h1>
+    </div>
+    <div id="main" style="background-color:#1ed760; display:flex; flex-flow:column; text-align:center; justify-content:center;">
+        main area
+    </div>
+</div>
+
+<style>
+    .sidebar {
+        background-color: #1DB954;
+        text-align: center;
+        justify-content: center;
+        display: flex;
+        flex-flow: column;
+    }
+</style>
